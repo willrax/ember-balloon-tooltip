75737d39f951015ddaac256ec99ea7d9ddaa4ef0
Will Raxworthy <me@willrax.com>
2018-02-08 11:31:20 +0000

    use correct importing of css files

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const path = require('path');
+
 module.exports = {
   name: 'ember-balloon-tooltip',
 
   included(app) {
-    app.import('node_modules/balloon-css/balloon.min.css');
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/balloon-css/balloon.min.css');
+  },
+
+  treeForVendor(vendorTree) {
+    let trees = [];
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    let cssPath = path.join(this.project.root, 'node_modules', 'balloon-css');
+
+    trees.push(new Funnel(cssPath, {
+      destDir: 'balloon-css',
+      files: ['balloon.min.css']
+    }));
+
+    return mergeTrees(trees);
   }
 };
